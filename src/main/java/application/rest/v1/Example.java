2e7e82b1b009b800a14fcfd861f08668cc43ffae
Soumen <soumdhar@in.ibm.com>
2017-11-23 13:32:27 +0530

    returning to 0th state

--- src/main/java/application/rest/v1/Example.java
+++ src/main/java/application/rest/v1/Example.java
@@ -16,7 +16,7 @@ public class Example {
   public @ResponseBody ResponseEntity<String> example() {
     List<String> list = new ArrayList<>();
     //return a simple list of strings
-    list.add("Congratulations, your application is up and running with CICD with Git update post helm");
+    list.add("Congratulations, your application is up and running");
     return new ResponseEntity<String>(list.toString(), HttpStatus.OK);
   }
 

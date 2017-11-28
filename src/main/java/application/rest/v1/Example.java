8f598ea98037d4eb3bf1b5006be29b3392f201b1
Soumen <soumdhar@in.ibm.com>
2017-11-28 18:17:51 +0530

    Updating for Swagger

--- src/main/java/application/rest/v1/Example.java
+++ src/main/java/application/rest/v1/Example.java
@@ -1,10 +1,12 @@
 package application.rest.v1;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ResponseBody;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -12,7 +14,7 @@ import java.util.List;
 public class Example {
 
 
-  @RequestMapping("v1/")
+  @RequestMapping(value = "v1/", method= RequestMethod.GET, produces = "application/json")
   public @ResponseBody ResponseEntity<String> example() {
     List<String> list = new ArrayList<>();
     //return a simple list of strings

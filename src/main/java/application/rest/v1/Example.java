5b58baf6b745438bd87a6afcfca275e7887b6180
Soumen <soumdhar@in.ibm.com>
2017-11-29 07:46:25 +0530

    Moved to JSON response

--- src/main/java/application/rest/v1/Example.java
+++ src/main/java/application/rest/v1/Example.java
@@ -8,18 +8,26 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 public class Example {
 
 
   @RequestMapping(value = "v1/", method= RequestMethod.GET, produces = "application/json")
-  public @ResponseBody ResponseEntity<String> example() {
-    List<String> list = new ArrayList<>();
+  //public @ResponseBody ResponseEntity<String> example() 
+  public Map<String,String> example()
+  {
+	  
+    //List<String> list = new ArrayList<>();
     //return a simple list of strings
-    list.add("Congratulations, your application is up and running");
-    return new ResponseEntity<String>(list.toString(), HttpStatus.OK);
+    //list.add("Congratulations, your application is up and running");
+    //return new ResponseEntity<String>(list.toString(), HttpStatus.OK);
+    Map<String,String> result = new HashMap<>();
+	result.put("message", "Congratulations, your application is up and running");
+	return result;
   }
 
 

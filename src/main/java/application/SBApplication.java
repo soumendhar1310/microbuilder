4eb7b3cfccc44025b78703e49c1610b737261b6d
Soumen <soumdhar@in.ibm.com>
2017-12-06 10:21:14 +0530

    Trying logback

--- src/main/java/application/SBApplication.java
+++ src/main/java/application/SBApplication.java
@@ -1,5 +1,7 @@
 package application;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SBApplication {
 
     public static void main(String[] args) {
+    	
         SpringApplication.run(SBApplication.class, args);
     }
 }
--- src/main/java/application/rest/v1/Example.java
+++ src/main/java/application/rest/v1/Example.java
@@ -1,11 +1,15 @@
 package application.rest.v1;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import application.SBApplication;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -15,7 +19,7 @@ import java.util.Map;
 @RestController
 public class Example {
 
-
+	private final Logger logger = LoggerFactory.getLogger(Example.class);
   @RequestMapping(value = "v1/", method= RequestMethod.GET, produces = "application/json")
   //public @ResponseBody ResponseEntity<String> example() 
   public Map<String,String> example()
@@ -27,6 +31,7 @@ public class Example {
     //return new ResponseEntity<String>(list.toString(), HttpStatus.OK);
     Map<String,String> result = new HashMap<>();
 	result.put("message", "Congratulations, your application is up and running");
+	logger.info("Log from Program to Logstash "+result);
 	return result;
   }
 
